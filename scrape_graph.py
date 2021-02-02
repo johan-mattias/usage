65c14c46566b512dec3d6b2c44b4dd337cbf6a74
johan-mattias <johan.snider@gmail.com>
2021-02-02 16:28:36 +0100

    converting areas of usage time to minutes

--- scrape_graph.py
+++ scrape_graph.py
@@ -28,66 +28,70 @@ def array_equal_set(graph_point, color_list):
             return True
     return False
 
-def find_base(graph, color_list):
-    gaps = []
-    tops = []
+def search_start_point(graph, color_list):
     for x in range(len(graph[0])):
         for y in range(len(graph)):
             if array_equal_set(graph[y][x], color_list):
-                print(x, y)
-                #go down
-                y1 = y
-                flag = True
-                while flag:
-                    if array_equal_set(graph[y1][x], color_list):
-                        y1 += 1
-                    else:
-                        y1 -= 1
-                        flag = False
-                tops.append(y1-y)
-                gaps.append(x)
-                #go right all the way to end
+                return(x, y)
 
-                #TODO: detect end of graph
-                while x < 475:
-                    print('x', x)
-                    x1 = x
-                    flag = True
-                    while flag:
-                        if array_equal_set(graph[y1][x1], color_list):
-                            x1 += 1
-                        else:
-                            print(x1, y1)
-                            flag = False
-                    x2 = x1
-                    flag = True
-                    while flag:
-                        if not array_equal_set(graph[y1][x2], color_list):
-                            print('x2', x2)
-                            x2 += 1
-                        else:
-                            print(x2, y1)
-                            print(x2-x1)
-                            gaps.append(x2-x1)
-                            flag = False
 
-                    y2 = y1
-                    flag = True
-                    while flag:
-                        if array_equal_set(graph[y2][x2], color_list):
-                            print('y2', y2)
-                            y2 -= 1
-                        else:
-                            print(x2, y2)
-                            print(y1-y2)
-                            tops.append(y1-y2)
-                            flag = False
-                    x = x2
-                    print(gaps)
-                    print(len(gaps))
-                    print(tops)
-                    print(len(tops))
-                return(gaps, tops)   
+def find_base(graph, color_list):
+    gaps = []
+    tops = []
+    starts = []
+    x, y = search_start_point(graph, color_list)
+    #print(x, y)
+    #go down
+    y1 = y
+    flag = True
+    while flag:
+        if array_equal_set(graph[y1][x], color_list):
+            y1 += 1
+        else:
+            y1 -= 1
+            flag = False
+    tops.append(y1-y)
+    gaps.append(x)
+    starts.append(x)
+    #go right all the way to end
+
+    #TODO: detect end of graph
+    while x < 475:
+        #print('x', x)
+        x1 = x
+        flag = True
+        while flag:
+            if array_equal_set(graph[y1][x1], color_list):
+                x1 += 1
+            else:
+                #print(x1, y1)
+                flag = False
+        x2 = x1
+        flag = True
+        while flag:
+            if not array_equal_set(graph[y1][x2], color_list):
+                #print('x2', x2)
+                x2 += 1
+            else:
+                #print(x2, y1)
+                #print(x2-x1)
+                gaps.append(x2-x1)
+                starts.append(x2)
+                flag = False
+
+        y2 = y1
+        flag = True
+        while flag:
+            if array_equal_set(graph[y2][x2], color_list):
+                #print('y2', y2)
+                y2 -= 1
+            else:
+                #print(x2, y2)
+                #print(y1-y2)
+                tops.append(y1-y2)
+                flag = False
+        x = x2
+    return(gaps, tops, starts)   
 
 
 
@@ -103,11 +107,34 @@ def run():
 
     width = blue_width
 
-    gaps, tops = find_base(graph, color_list)
-    print(gaps)
-    print(tops)
+    gaps, tops, starts = find_base(graph, color_list)
+    print(len(gaps), gaps)
+    print(len(tops), tops)
+    print(len(starts), starts)
     
-    
+    print(list(zip(starts, tops)))
+
+    areas = []
+
+    for top in tops:
+        areas.append(top*15)
+
+    print(areas)
+
+    sum = 0
+
+    for area in areas:
+        sum += area
+    print('area:', sum)
+    #417 minutes
+    print('pixels per minute', round(sum/417))
+
+    pix_per_min = round(sum/417)
+
+    times = []
+    for area in areas:
+        times.append(round(area/pix_per_min))
+    print(times)
 
 
 
