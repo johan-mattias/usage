f04b6b79e80f9699a3792585493142ac4f49fea6
johan-mattias <johan.snider@gmail.com>
2021-02-03 14:09:58 +0100

    cleaned up files and using tess on the entire screen shots, really good results so far

--- tess.py
+++ tess.py
@@ -1,4 +1,22 @@
 from skimage import io
 import pytesseract
-time = io.imread('summary.png')
-print(pytesseract.image_to_string(time))
+import glob
+
+
+time = io.imread('IMG-1800.PNG')
+
+images = glob.glob('usage/*.PNG')
+
+print(images)
+
+
+for image_path in images:
+    pic = io.imread(image_path)
+    text = pytesseract.image_to_string(pic)
+
+    image_name = image_path.split('/')
+    image_path = image_name[1].replace('.PNG', '.txt')
+    with open('out/'+image_path, "w") as file:
+        file.write(text)
+
+#print(pytesseract.image_to_string(time))
